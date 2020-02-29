cc777b51703cde9baa21eb5d74bfb2815bbb6984
James Rogers <jmr172@utulsa.edu>
2020-02-29 08:31:13 -0500

    Add files via upload

---
+++ Project_01/Euclidean.java
@@ -0,0 +1,41 @@
+/*
+ * Created By:			  James Rogers
+ * Created On:			  02/24/2020
+ * Last Modified By:	James Rogers
+ * Last Modified On:	02/24/2020
+ * Purpose:			      Calculate euclidean distance between all points in array
+ */
+
+public class Euclidean {
+
+  // Constructor for the class
+  public Euclidean() {}
+
+  // Calculate euclidean distance between every point
+  public void calc(double[][] input, double[][] output, int input_size) {
+    int counter = 0;
+    for (int i = 0; i < input_size; i++) {
+      for (int j = i + 1; j < input_size; j++) {
+        double x_diff = input[i][0] - input[j][0];
+        double y_diff = input[i][1] - input[j][1];
+        double distance = Math.sqrt((x_diff * x_diff) + (y_diff * y_diff));
+        // I'm rounding distance to 3 decimal places for aesthetic reasons
+        // Please let me know if this isn't acceptable
+        distance = Math.floor(distance * 1000) / 1000;
+        output[counter][0] = i;
+        output[counter][1] = j;
+        output[counter][2] = distance;
+        counter++;
+      }
+    }
+  }
+
+  // Find the smallest entry in the array
+  public int find(double[][] input, int input_size) {
+    int index = 0;
+    for (int i = 0; i < input_size; i++) {
+      if (input[index][2] > input[i][2]) index = i;
+    }
+    return index;
+  }
+}
